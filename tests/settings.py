7ba6d4c12ac62590df3f35c2cc5652b9abc8b605
Del Hyman-Jones <dhymanjones@gmail.com>
2019-01-14 12:15:10 +0000

    removed middleware as it is not needed

--- tests/settings.py
+++ tests/settings.py
@@ -47,7 +47,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'idempotency_key.middleware.IdempotencyKeyMiddleware',
 ]
 
 ROOT_URLCONF = 'tests.urls'
