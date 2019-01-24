8229d874bbd6f302b1a38914bd22ed214ce8d1b6
Del Hyman-Jones <dhymanjones@gmail.com>
2019-01-24 14:59:24 +0000

    Added test cases to improve coverage.

--- tests/tests/test_utils.py
+++ tests/tests/test_utils.py
@@ -1,4 +1,7 @@
-from idempotency_key.utils import idempotency_key_exists, idempotency_key_response
+from django.test import override_settings
+
+from idempotency_key import status
+from idempotency_key.utils import idempotency_key_exists, idempotency_key_response, get_store_on_statuses
 
 
 class Request(object):
@@ -36,3 +39,30 @@ def test_idempotency_key_response_object():
     response = Response()
     request.idempotency_key_response = response
     assert idempotency_key_response(request) is response
+
+
+@override_settings(
+    IDEMPOTENCY_KEY={
+        'STORE_ON_STATUSES': [status.HTTP_200_OK]
+    }
+)
+def test_get_store_on_statuses_default():
+    assert get_store_on_statuses() == [
+        status.HTTP_200_OK
+    ]
+
+
+@override_settings(
+    IDEMPOTENCY_KEY={}
+)
+def test_get_store_on_statuses_not_specified():
+    assert get_store_on_statuses() == [
+        status.HTTP_200_OK,
+        status.HTTP_201_CREATED,
+        status.HTTP_202_ACCEPTED,
+        status.HTTP_203_NON_AUTHORITATIVE_INFORMATION,
+        status.HTTP_204_NO_CONTENT,
+        status.HTTP_205_RESET_CONTENT,
+        status.HTTP_206_PARTIAL_CONTENT,
+        status.HTTP_207_MULTI_STATUS,
+    ]
