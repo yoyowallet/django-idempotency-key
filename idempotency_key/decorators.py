a163bbbd1d506886e7e7a8a0403c9551059be424
Del Hyman-Jones <dhymanjones@gmail.com>
2019-01-30 09:58:14 +0000

    Change the @idempotency_key decorator to allow for a cache_name parameter so that the storage cache can be specified per view function and allows different cache settings to be specified.

--- idempotency_key/decorators.py
+++ idempotency_key/decorators.py
@@ -13,16 +13,23 @@ from functools import wraps
 #   ...
 
 
-def idempotency_key(view_func):
-    """
-    Mark a view function as requiring idempotency key protection but the view should control the response.
-    """
+def idempotency_key(*args, cache_name=None):
+    def _idempotency_key(view_func):
+        """
+        Mark a view function as requiring idempotency key protection but the view should control the response.
+        """
 
-    def wrapped_view(*args, **kwargs):
-        return view_func(*args, **kwargs)
+        def wrapped_view(*args, **kwargs):
+            return view_func(*args, **kwargs)
 
-    wrapped_view.idempotency_key = True
-    return wraps(view_func)(wrapped_view)
+        wrapped_view.idempotency_key = True
+        if cache_name:
+            wrapped_view.idempotency_key_cache_name = cache_name
+        return wraps(view_func)(wrapped_view)
+
+    if len(args) > 0 and callable(args[0]):
+        return _idempotency_key(args[0])
+    return _idempotency_key
 
 
 def idempotency_key_exempt(view_func):
