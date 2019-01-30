31ce6020a7cc0fbcec8677d57f0b325a52537be3
Del Hyman-Jones <dhymanjones@gmail.com>
2019-01-30 10:00:44 +0000

    Changed the storage settings so that they now appear under {IDEMPOTENCY_KEY: {'STORAGE': {...} } }

--- idempotency_key/utils.py
+++ idempotency_key/utils.py
@@ -12,11 +12,6 @@ def idempotency_key_response(request):
     return getattr(request, 'idempotency_key_response', None)
 
 
-def get_storage_class():
-    idkey_settings = getattr(settings, 'IDEMPOTENCY_KEY', dict())
-    return module_loading.import_string(idkey_settings.get('STORAGE_CLASS', 'idempotency_key.storage.MemoryKeyStorage'))
-
-
 def get_encoder_class():
     idkey_settings = getattr(settings, 'IDEMPOTENCY_KEY', dict())
     return module_loading.import_string(idkey_settings.get('ENCODER_CLASS', 'idempotency_key.encoders.BasicKeyEncoder'))
@@ -27,9 +22,33 @@ def get_conflict_code():
     return idkey_settings.get('CONFLICT_STATUS_CODE', status.HTTP_409_CONFLICT)
 
 
-def get_cache_name():
+def get_storage_class():
     idkey_settings = getattr(settings, 'IDEMPOTENCY_KEY', dict())
-    return idkey_settings.get('CACHE_NAME', 'default')
+    storage_settings = idkey_settings.get('STORAGE', dict())
+    return module_loading.import_string(storage_settings.get('CLASS', 'idempotency_key.storage.MemoryKeyStorage'))
+
+
+def get_storage_cache_name():
+    idkey_settings = getattr(settings, 'IDEMPOTENCY_KEY', dict())
+    storage_settings = idkey_settings.get('STORAGE', dict())
+    return storage_settings.get('CACHE_NAME', 'default')
+
+
+def get_storage_store_on_statuses():
+    idkey_settings = getattr(settings, 'IDEMPOTENCY_KEY', dict())
+    storage_settings = idkey_settings.get('STORAGE', dict())
+    return storage_settings.get(
+        'STORE_ON_STATUSES', [
+            status.HTTP_200_OK,
+            status.HTTP_201_CREATED,
+            status.HTTP_202_ACCEPTED,
+            status.HTTP_203_NON_AUTHORITATIVE_INFORMATION,
+            status.HTTP_204_NO_CONTENT,
+            status.HTTP_205_RESET_CONTENT,
+            status.HTTP_206_PARTIAL_CONTENT,
+            status.HTTP_207_MULTI_STATUS,
+        ]
+    )
 
 
 def get_lock_class():
@@ -60,19 +79,3 @@ def get_lock_name():
     idkey_settings = getattr(settings, 'IDEMPOTENCY_KEY', dict())
     lock_settings = idkey_settings['LOCK'] if 'LOCK' in idkey_settings else dict()
     return lock_settings.get('NAME', 'MyLock')
-
-
-def get_store_on_statuses():
-    idkey_settings = getattr(settings, 'IDEMPOTENCY_KEY', dict())
-    return idkey_settings.get(
-        'STORE_ON_STATUSES', [
-            status.HTTP_200_OK,
-            status.HTTP_201_CREATED,
-            status.HTTP_202_ACCEPTED,
-            status.HTTP_203_NON_AUTHORITATIVE_INFORMATION,
-            status.HTTP_204_NO_CONTENT,
-            status.HTTP_205_RESET_CONTENT,
-            status.HTTP_206_PARTIAL_CONTENT,
-            status.HTTP_207_MULTI_STATUS,
-        ]
-    )
